da49abbc3876e6eaa2caf8e3fe44a643e3031d91
diwan08 <diwanart898@gmail.com>
2022-06-30 12:28:10 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>membuat form</title>    
+</head>
+<body>
+    <script src="halo.js"></script>
+<form action="form.html" method="get">
+    nama: <input type="text" name="nama" value="nama kamu" />
+<br />
+password: <input type="password"  name="password" />
+<br>
+jenis kelamin :
+<input type="radio"  name="jenis_kelamin"
+value="laki-laki" checked />
+laki-laki
+<input type="radio" name="jenis_kelamin"
+value="perempuan"/>
+perempuan
+<br>
+<input type="submit" value="mulai proses" >
+<input type="reset" value="hapus form" >
+</form>   
+</body>    
+</html>
